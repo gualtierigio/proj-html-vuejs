2d37dc919fd3382a3dab4c42dbe1ba525db55266
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-10-06 17:20:28 +0200

    make navbar as component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,17 @@
 <template>
     <header id="jumbotron-header">
-      <div class="d-flex" id="top-header">
-        <a href="#"><h1>Everlead<span class="red-dot">.</span></h1></a>
-        <nav>
-          <ul>
-            <li id="collaps-menu" v-for="(element, index) in navMenu" :key="index">
-              <a href="{link.url}">
-                <div id="arrow">
-                  <i class="fas fa-long-arrow-alt-right"></i>
-                </div>{{element.text}}
-              </a>
-            </li>
-            <li>
-              <a href="#"><i class="fas fa-search"></i></a>
-            </li>
-            <li>
-              <a href="#"><i class="fas fa-bars"></i></a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <Navbar />
     </header>
 </template>
 
 <script>
+import Navbar from './Navbar.vue';
+
 export default {
   name: "Header",
+  components: {
+    Navbar
+  },
   data() {
     return {
       navMenu: [
@@ -71,58 +57,6 @@ export default {
   height: 95vh;
   background-size: cover;
   background-position: center;
-}
-
-#top-header{
-  width: 100%;
-  height: 160px;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 30px;
-}
-
-nav{
-  text-transform: uppercase;
-  ul{
-    li{
-      display: inline-block;
-      padding: 0 20px;
-      a{
-        div{
-        width: 20px;
-        display: inline-block;
-        i{
-          display: none;
-        }
-      }
-      }
-      a:hover{
-        color: $red-color;
-        div i{
-          display: block;
-        }
-      }
-    }
-  }
-}
-
-a:hover{
-  color: $red-color;
-}
-
-@media screen and (max-width: 1150px) {
-  #collaps-menu{
-    display: none;
-  }
-}
-
-a{
-  text-decoration: none;
-  color: $total-black;
-}
-
-.red-dot{
-  color: $red-color;
 }
 
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="d-flex" id="top-header">
+        <a href="#"><h1>Everlead<span class="red-dot">.</span></h1></a>
+        <nav>
+          <ul>
+            <li id="collaps-menu" v-for="(element, index) in navMenu" :key="index">
+              <a href="{link.url}">
+                <div>
+                  <i class="fas fa-long-arrow-alt-right"></i>
+                </div>{{element.text}}
+              </a>
+            </li>
+            <li>
+              <a href="#"><i class="fas fa-search"></i></a>
+            </li>
+            <li>
+              <a href="#"><i class="fas fa-bars"></i></a>
+            </li>
+          </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      navMenu: [
+        {
+          text: "home",
+          url: "#home",
+        },
+        {
+          text: "pages",
+          url: "#pages",
+        },
+        {
+          text: "blog",
+          url: "#blog",
+        },
+        {
+          text: "shop",
+          url: "#shop",
+        },
+        {
+          text: "events",
+          url: "#events",
+        },
+        {
+          text: "elements",
+          url: "#elements",
+        }
+      ]
+    }
+  }
+
+};
+</script>
+
+
+<style scoped lang="scss">
+@import '../style/general.scss';
+@import '../style/variables.scss';
+
+#top-header{
+  width: 100%;
+  height: 160px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 30px;
+}
+
+nav{
+  text-transform: uppercase;
+  ul{
+    li{
+      display: inline-block;
+      padding: 0 20px;
+      a{
+        div{
+        width: 20px;
+        display: inline-block;
+        i{
+          display: none;
+        }
+      }
+      }
+      a:hover{
+        color: $red-color;
+        div i{
+          display: block;
+        }
+      }
+    }
+  }
+}
+
+a:hover{
+  color: $red-color;
+}
+
+@media screen and (max-width: 1150px) {
+  #collaps-menu{
+    display: none;
+  }
+}
+
+a{
+  text-decoration: none;
+  color: $total-black;
+}
+
+.red-dot{
+  color: $red-color;
+}
+
+</style>
