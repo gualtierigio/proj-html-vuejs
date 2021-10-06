56988d63f8cdeade0825a40f8f0404df6ad2eff8
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-10-06 14:38:03 +0200

    start with header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,28 @@
 <template>
-    <header></header>
+    <header id="jumbotron-header">
+
+    </header>
 </template>
 
 <script>
 export default {
   name: "Header",
 
+
 };
 </script>
 
 
 <style scoped lang="scss">
+@import '../style/general.scss';
+@import '../style/variables.scss';
+
+#jumbotron-header{
+  background-image: url(../assets/img/rev-slider-main-home-img-03.jpg);
+  width: 100%;
+  height: 95vh;
+  background-size: cover;
+  background-position: center;
+}
 
 </style>
