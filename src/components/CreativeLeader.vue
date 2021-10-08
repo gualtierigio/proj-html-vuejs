1c374c4c2e6511ac004d5a37aad6ac2e324ed925
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-10-08 16:44:19 +0200

    creative leader complete

--- src/components/CreativeLeader.vue
+++ src/components/CreativeLeader.vue
@@ -1,12 +1,46 @@
 <template>
     <div class="page-wrap">
         <div class="container">
-            <div class="row">
+            <div class="row gx-5">
                 <div class="col-6">
                     <h3>Creative Leader<span class="red-dot">.</span></h3>
-                    <div class="red-line"></div>
+                    <div class="red-line static"></div>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt nemo illo quo culpa consectetur laborum ullam quam esse magni quia maiores, nostrum natus, iure eaque numquam perferendis, porro doloremque blanditiis?</p>
                     <h5 class="more">read more</h5>
+                </div>
+                <div class="col-6 data-percent">
+                    <div class="mb-4">
+                        <div class="d-flex justify-content-between">
+                            <h6>Mentorship</h6>
+                            <h6>78%</h6>
+                        </div>
+                        <div class="red-line percent mentorship"></div>
+                        <div class="full-percent"></div>
+                    </div>
+                    <div class="mb-4">
+                        <div class="d-flex justify-content-between">
+                            <h6>Education</h6>
+                            <h6>95%</h6>
+                        </div>
+                        <div class="red-line percent education"></div>
+                        <div class="full-percent"></div>
+                    </div>
+                    <div class="mb-4">
+                        <div class="d-flex justify-content-between">
+                            <h6>Learning</h6>
+                            <h6>65%</h6>
+                        </div>
+                        <div class="red-line percent learning"></div>
+                        <div class="full-percent"></div>
+                    </div>
+                    <div>
+                        <div class="d-flex justify-content-between">
+                            <h6>Motivation</h6>
+                            <h6>83%</h6>
+                        </div>
+                        <div class="red-line percent motivation"></div>
+                        <div class="full-percent"></div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -30,10 +64,46 @@ export default {
 }
 
 .red-line{
-  width: 100px;
-  height: 2px;
   background-color: $red-color;
-  margin: 15px 0;
+}
+
+.static{
+    width: 100px;
+    margin: 15px 0;
+    height: 2px;
+}
+
+.data-percent{
+    padding: 0 80px;
+}
+
+// red line in percent
+
+.mentorship{
+    width: 78%;
+}
+
+.education{
+    width: 95%;
+}
+
+.learning{
+    width: 65%;
+}
+
+.motivation{
+    width: 83%;
+}
+
+.percent{
+    height: 4px;
+}
+
+.full-percent{
+    opacity: 0.3;
+    width: 100%;
+    height: 1px;
+    background-color: $black-color;
 }
 
 .more{
